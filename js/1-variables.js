5bfcd0800fac17108058130e41ab44955bff462e
Joaquin Cortes Carbajo <joaquincortescarbajo@gmail.com>
2022-06-29 18:07:55 -0300

    mostramos diferentes mensajes por pantalla, consola y alert

---
+++ js/1-variables.js
@@ -0,0 +1,18 @@
+// Esto es un comentario de una sola linea
+
+/* Esto es un comentario 
+de multiples lines */
+
+// mostrar un mensaje en el documento html o pantalla 
+document.write('<h1>Hola mundo, año 2022, canción</h1>');
+
+// mostrar un mensaje por consola 
+console.log('alguan frase');
+
+// mostrar un mensaje por una ventana emergente
+// alert('esto es una ventana emergente');
+
+// mostrar por pantalla la suma de dos numero 29 + 6
+document.write('<p>Resultado: ' + (29 + 6) +'</p>');
+
+// variables 
